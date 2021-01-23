abfb6d750190050d517714bbf451ddc3c3f579eb
Jack Clayton <jackos@me.com>
2021-01-23 12:39:36 +0800

    Fish alias and functions

--- .config/fish/functions/code.fish
+++ .config/fish/functions/code.fish
@@ -1,4 +1,3 @@
-# Defined in - @ line 1
-#function code --wraps=code-insiders --description 'alias code code-insiders'
-#  code-insiders  $argv;
-#end
+function code --wraps=code-insiders --description 'alias code code-insiders'
+  code-insiders  $argv;
+end
